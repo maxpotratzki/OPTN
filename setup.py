09f21a4acbe03a03eae90f85c4bec6847e59ba22
maxpotratzki <m.potratzki47@gmail.com>
2025-04-02 11:09:55 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optn",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Autoren",
+    author_email="m.potratzki47@gmail.com",
+    description="Toolbox for creating and analyzing OPTN's and TTN's",
+    url="https://github.com/yourusername/OPTN",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
